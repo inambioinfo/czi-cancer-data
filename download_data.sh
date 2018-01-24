bbf55682830b78f9365690d0c32c25df885643dc
Greg Way <gregory.way@gmail.com>
2018-01-24 16:12:57 -0500

    Download Initial Data (#2)
    * convert license to markdown
    
    * remove license file
    
    * add initial download script
    
    * ignore data directory
    
    * change to BSD-3-Clause name
    
    * update to BSD-3-Clause text
    
    * add CC-BY license
    
    * add brief description of datasets
    
    also remove meyer et al

---
+++ download_data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# czi-cancer-data
+# 2018 Gregory Way
+
+# Script to download gene expression and covariate data from 4 single cell gene
+# expression analyses. The script will fetch, decompress, and rename each data set
+# listed below
+
+mkdir --parents data/raw
+
+# Patel et al. 2014 (https://doi.org/10.1126/science.1254257)
+# RNAseq (SMART-seq) of 430 Glioblastoma Cells from 5 patients
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57872/suppl/GSE57872_GBM_data_matrix.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57872/matrix/GSE57872_series_matrix.txt.gz
+gunzip -c GSE57872_GBM_data_matrix.txt.gz > data/raw/GSE57872_data.txt
+gunzip -c GSE57872_series_matrix.txt.gz > data/GSE57872_samples.txt
+rm *.gz
+
+# Sottoriva et al. 2013 (https://doi.org/10.1073/pnas.1219747110)
+# Microarray of 11 Glioblastoma patients (multi-sampled to test intratumoral heterogeneity)
+wget http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-1129/E-MTAB-1129.raw.1.zip
+wget http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-1129/E-MTAB-1129.sdrf.txt
+unzip E-MTAB-1129.raw.1.zip 
+mv Genomestudio-GBMrawdata.txt data/raw/E-MTAB-1129_data.txt
+mv E-MTAB-1129.sdrf.txt data/E-MTAB-1129_samples.txt
+
+# Venteicher et al. 2017 (https://doi.org/10.1126/science.aai8478)
+# Tirosh et al. 2016 (https://doi.org/10.1038/nature20123)
+# RNAseq (Smart-seq2) profiling of two low-grade glioma single cell datasets
+
+# Astrocytoma Data
+# 9,879 Cells from 10 Astrocytoma patients
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE89nnn/GSE89567/suppl/GSE89567_IDH_A_processed_data.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE89nnn/GSE89567/matrix/GSE89567_series_matrix.txt.gz
+gunzip -c GSE89567_IDH_A_processed_data.txt.gz > data/raw/GSE89567_data.txt
+gunzip -c GSE89567_series_matrix.txt.gz > data/GSE89567_samples.txt
+
+# Oligodendroglioma Data
+# 4,347 Cells from 6 Oligodendroglioma patients
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70630/suppl/GSE70630_OG_processed_data_v2.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70630/matrix/GSE70630_series_matrix.txt.gz
+gunzip -c GSE70630_OG_processed_data_v2.txt.gz > data/raw/GSE70630_data.txt
+gunzip -c GSE70630_series_matrix.txt.gz > data/GSE70630_samples.txt
+rm *.gz
