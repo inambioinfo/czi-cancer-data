47d9fc9bd1a796cad5a32b2976b4e318c50b8d74
Greg Way <gregory.way@gmail.com>
2018-01-25 11:19:49 -0500

    closes #3 (#5)
    adding tirosh et al melanoma data

--- download_data.sh
+++ download_data.sh
@@ -15,7 +15,6 @@ wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57872/suppl/GSE57872_GBM_
 wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57872/matrix/GSE57872_series_matrix.txt.gz
 gunzip -c GSE57872_GBM_data_matrix.txt.gz > data/raw/GSE57872_data.txt
 gunzip -c GSE57872_series_matrix.txt.gz > data/GSE57872_samples.txt
-rm *.gz
 
 # Sottoriva et al. 2013 (https://doi.org/10.1073/pnas.1219747110)
 # Microarray of 11 Glioblastoma patients (multi-sampled to test intratumoral heterogeneity)
@@ -26,7 +25,7 @@ mv Genomestudio-GBMrawdata.txt data/raw/E-MTAB-1129_data.txt
 mv E-MTAB-1129.sdrf.txt data/E-MTAB-1129_samples.txt
 
 # Venteicher et al. 2017 (https://doi.org/10.1126/science.aai8478)
-# Tirosh et al. 2016 (https://doi.org/10.1038/nature20123)
+# Tirosh et al. 2016a (https://doi.org/10.1038/nature20123)
 # RNAseq (Smart-seq2) profiling of two low-grade glioma single cell datasets
 
 # Astrocytoma Data
@@ -42,4 +41,13 @@ wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70630/suppl/GSE70630_OG_p
 wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70630/matrix/GSE70630_series_matrix.txt.gz
 gunzip -c GSE70630_OG_processed_data_v2.txt.gz > data/raw/GSE70630_data.txt
 gunzip -c GSE70630_series_matrix.txt.gz > data/GSE70630_samples.txt
+
+# Tirosh et al. 2016b (https://doi.org/10.1126/science.aad0501)
+# RNAseq (Smart-seq2) of 4645 Cells from 19 melanoma patients
+# This analysis includes malignant, immune, stromal, and endothelial cells
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE72nnn/GSE72056/suppl/GSE72056_melanoma_single_cell_revised_v2.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE72nnn/GSE72056/matrix/GSE72056_series_matrix.txt.gz
+gunzip -c GSE72056_melanoma_single_cell_revised_v2.txt.gz > data/raw/GSE72056_data.txt
+gunzip -c GSE72056_series_matrix.txt.gz > data/GSE72056_samples.txt
 rm *.gz
+rm *.zip
